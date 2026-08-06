8dff6504b1d4c54f440bce64d0eb2a538111ebde
gujaratharva021-lgtm <gujaratharva021@gmail.com>
2026-08-06 10:49:51 +0530

    fix: point delivery_app to deployed backend URL instead of local IP

--- delivery_app/lib/services/api_service.dart
+++ delivery_app/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://192.168.1.18:8081/api/v1';
+  static const String baseUrl = 'https://ecommerce-backend-dd4u.onrender.com/api/v1';
 
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
